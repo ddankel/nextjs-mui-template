62e248e661a1b752f0b6f8791c9e68bc525c5718
Dave Dankel <ddankel@gmail.com>
2022-10-05 15:09:41 -0500

    Initialize Link page

--- pages/links.js
+++ pages/links.js
@@ -1,7 +1,60 @@
-import { Container } from "@mui/material";
+import CodeBlock from "@/ui/CodeBlock";
+import Link from "@/ui/Link";
+import { Box, Container, Stack, Typography } from "@mui/material";
+import { styled } from "@mui/system";
+
+const VisualDemo = styled(Box)``;
+VisualDemo.defaultProps = {
+  border: 1,
+  borderRadius: 1,
+  borderColor: "grey.700",
+  padding: 2,
+  my: 2,
+};
 
 const LinksPage = () => {
-  return <Container>Links</Container>;
+  return (
+    <Container>
+      <Stack spacing={5}>
+        <Box>
+          <Typography variant="h1">Links</Typography>
+          <Typography variant="body1">asdf</Typography>
+        </Box>
+
+        <Box>
+          <Typography variant="h2">Internal Link</Typography>
+          <Typography variant="body1">asdf</Typography>
+          <VisualDemo>
+            <Link href="/">Home</Link>
+          </VisualDemo>
+          <CodeBlock>{`<Link href="/">Home</Link>`}</CodeBlock>
+        </Box>
+
+        <Box>
+          <Typography variant="h2">Explicit External Link</Typography>
+          <Typography variant="body1">asdf</Typography>
+          <VisualDemo>
+            <Link href="/" external>
+              Home
+            </Link>
+            <Link href="https://github.com" external>
+              Home
+            </Link>
+          </VisualDemo>
+          <CodeBlock>{`<Link href="/">Home</Link><Link href="/">Home</Link>`}</CodeBlock>
+        </Box>
+
+        <Box>
+          <Typography variant="h2">Implicit External Link</Typography>
+          <Typography variant="body1">asdf</Typography>
+          <VisualDemo>
+            <Link href="https://github.com">Home</Link>
+          </VisualDemo>
+          <CodeBlock>{`<Link href="/">Home</Link>`}</CodeBlock>
+        </Box>
+      </Stack>
+    </Container>
+  );
 };
 
 export default LinksPage;
--- src/components/ui/Link.js
+++ src/components/ui/Link.js
@@ -2,11 +2,19 @@ import { forwardRef } from "react";
 import MuiLink from "@mui/material/Link";
 import NextLink from "next/link";
 
+const isExternal = (url) => {
+  if (typeof window === "undefined") return false;
+
+  const base = new URL(`${window.location.protocol}//${window.location.host}`);
+  return new URL(url, base).hostname !== base.hostname;
+};
+
 /**
  * Wrapper to combine MUI and NextJS Link components
  *
  * Modified Ben's code to add an 'external' prop which if set will just cause
- * the MUI portion to render.
+ * the MUI portion to render.  If external is not explicitly set, the url will
+ * be inspected and it's internal/external status inferred from it.
  *
  * @see https://www.benmvp.com/blog/wrapping-next-link-custom-ui-link-component/
  *
@@ -19,7 +27,13 @@ import NextLink from "next/link";
 const Link = forwardRef(function Link(props, ref) {
   const { href, prefetch, replace, scroll, shallow, locale, external, ...muiProps } = props;
 
-  if (external) return <MuiLink ref={ref} href={href} {...muiProps} />;
+  if (external === undefined) external = isExternal(href);
+
+  if (external) {
+    return (
+      <MuiLink ref={ref} href={href} target="_blank" rel="noopener noreferrer" {...muiProps} />
+    );
+  }
 
   return (
     <NextLink
