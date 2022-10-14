5fd468504c220e866ce8dd9818566d14a1a57b49
Dave Dankel <ddankel@gmail.com>
2022-10-14 14:50:22 -0500

    Fixed obsolete AppBar prop

--- src/layout/AppLayout/Masthead.js
+++ src/layout/AppLayout/Masthead.js
@@ -5,48 +5,56 @@ import Link from "@/ui/Link";
 import { styled } from "@mui/system";
 import navStructure from "./navStructure.json";
 
+const Offset = styled("div")(({ theme }) => ({
+  ...theme.mixins.toolbar,
+  marginBottom: theme.spacing(4),
+}));
+
 const MastLink = styled(Link)({ color: "white" });
 MastLink.defaultProps = { underline: "none" };
 
 const Masthead = ({ onOpenMenu }) => {
   return (
-    <AppBar component="nav" position="static" sx={{ mb: 4 }}>
-      <Container maxWidth="md">
-        <Toolbar>
-          <Typography
-            variant="h6"
-            component={MastLink}
-            href="/"
-            sx={{ flexGrow: 1, letterSpacing: "4px" }}
-          >
-            placeholder.com
-          </Typography>
+    <>
+      <AppBar component="nav" position="fixed">
+        <Container maxWidth="md">
+          <Toolbar>
+            <Typography
+              variant="h6"
+              component={MastLink}
+              href="/"
+              sx={{ flexGrow: 1, letterSpacing: "4px" }}
+            >
+              placeholder.com
+            </Typography>
 
-          <Stack
-            direction="row"
-            spacing={2}
-            component="nav"
-            sx={{ display: { xs: "none", sm: "block" } }}
-          >
-            {navStructure.map((navItem) => (
-              <MastLink href={navItem.to} external={navItem.external} key={navItem.label}>
-                {navItem.label}
-              </MastLink>
-            ))}
-          </Stack>
+            <Stack
+              direction="row"
+              spacing={2}
+              component="nav"
+              sx={{ display: { xs: "none", sm: "block" } }}
+            >
+              {navStructure.map((navItem) => (
+                <MastLink href={navItem.to} external={navItem.external} key={navItem.label}>
+                  {navItem.label}
+                </MastLink>
+              ))}
+            </Stack>
 
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={onOpenMenu}
-            sx={{ ml: 2, display: { sm: "none" } }}
-          >
-            <MenuIcon />
-          </IconButton>
-        </Toolbar>
-      </Container>
-    </AppBar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={onOpenMenu}
+              sx={{ ml: 2, display: { sm: "none" } }}
+            >
+              <MenuIcon />
+            </IconButton>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Offset />
+    </>
   );
 };
 
