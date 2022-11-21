5335e2726ac38860090bbda5484b489dc41a8a6e
Dave Dankel <ddankel@gmail.com>
2022-11-21 08:41:00 -0600

    Specified subset for google font

--- src/styles/muiTheme.js
+++ src/styles/muiTheme.js
@@ -1,9 +1,7 @@
 import { createTheme } from "@mui/material/styles";
 import { Roboto } from "@next/font/google";
 
-const roboto = Roboto({
-  weight: "400",
-});
+const roboto = Roboto({ weight: "400", subsets: ["latin"] });
 
 const muiTheme = createTheme({
   palette: {
