d3fa7fb4657d9199afa9a3a2bca86965ecb9245f
Dave Dankel <ddankel@gmail.com>
2023-03-16 23:53:40 -0500

    Bump dependencies
    - Bump next from 13.2.3 to 13.2.4 (#91)
    - Migrated from @next/font to next/font
    - Bump eslint-config-next from 13.2.3 to 13.2.4 (#89)
    - Bump eslint from 8.35.0 to 8.36.0 (#88)

--- src/styles/muiTheme.js
+++ src/styles/muiTheme.js
@@ -1,5 +1,5 @@
 import { createTheme } from "@mui/material/styles";
-import { Roboto } from "@next/font/google";
+import { Roboto } from "next/font/google";
 
 const roboto = Roboto({ weight: "400", subsets: ["latin"] });
 
