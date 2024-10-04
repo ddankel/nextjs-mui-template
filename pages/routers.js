65a23a0ffff572c026e4a1bc81a4123fedb39786
Dave Dankel <ddankel@gmail.com>
2024-10-04 15:59:33 -0500

    Add routers page discussing router use in this template

---
+++ pages/routers.js
@@ -0,0 +1,17 @@
+import PageTitle from "@/ui/PageTitle";
+import Paragraph from "@/ui/Paragraph";
+
+const RoutesPage = () => {
+  return (
+    <>
+      <PageTitle>Routers</PageTitle>
+
+      <Paragraph>
+        This template uses the app router for most content, but this page is a demo of how to use
+        the pages router with MUI.
+      </Paragraph>
+    </>
+  );
+};
+
+export default RoutesPage;
