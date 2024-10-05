e51458dbab7e29579984abd4211ac18b8f69a6ae
Dave Dankel <ddankel@gmail.com>
2024-10-05 02:04:00 -0500

    update _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,20 @@
+import { NextPage } from "next";
+import { AppProps } from "next/app";
+
 import AppLayout from "@/layout/AppLayout";
 import { muiTheme } from "@/styles/muiTheme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { AppCacheProvider } from "@mui/material-nextjs/v14-pagesRouter";
 
-export default function MyApp({ Component, pageProps, ...restProps }) {
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: React.ReactElement) => React.ReactNode;
+};
+
+interface AppPropsWithLayout extends AppProps {
+  Component: NextPageWithLayout
+}
+
+export default function MyApp({ Component, pageProps, ...restProps }:AppPropsWithLayout ) {
   return (
     <AppCacheProvider {...restProps}>
       <ThemeProvider theme={muiTheme}>
